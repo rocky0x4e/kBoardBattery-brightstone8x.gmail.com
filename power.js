df0fbc3fa276195ba4cca6ae2d9006f130059f9a
Thach Nguyen <thach.nguyen@cinatic.com>
2019-09-05 23:50:46 +0700

    fix constant %

--- power.js
+++ power.js
@@ -1,20 +1,17 @@
 const Lang = imports.lang;
-const UPower = new imports.gi.UPowerGlib.Device();
 const BaseIndicator = imports.ui.status.power.Indicator;
 
 var Indicator = class extends BaseIndicator {
    _getBatteryStatus() {
+	    var UPower = new imports.gi.UPowerGlib.Device();
 		UPower.set_object_path_sync("/org/freedesktop/UPower/devices/keyboard_hid_dco2co26ocdo21o79_battery", null);
-		
-	    const percentage = UPower.percentage + '%';
+		// UPower.refresh_sync(null);
+	    var percentage = UPower.percentage + '%';
 		this._percentageLabel.visible = true;
 
 		if (!UPower.has_statistics){
 			return "Pending...";
 		}
-
-      // Ensure percentage label is enabled regardless of gsettings
-
 
         return _("%s").format(percentage);
       }
