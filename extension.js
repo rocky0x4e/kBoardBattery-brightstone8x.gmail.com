4334fc0888a0001e48520e0210f427e611f90d3d
Thach Nguyen <thach.nguyen@cinatic.com>
2019-09-07 06:54:45 +0700

    new extension.js

--- extension.js
+++ extension.js
@@ -1,36 +1,39 @@
+const Lng = imports.lang
+const St = imports.gi.St;
+const Main = imports.ui.main;
+const Tweener = imports.ui.tweener;
+const Clutter = imports.gi.Clutter;
 const ExtensionUtils = imports.misc.extensionUtils;
-const BaTime = ExtensionUtils.getCurrentExtension();
-const Panel = imports.ui.main.panel;
+const Libs = ExtensionUtils.getCurrentExtension();
+const Timers    = Libs.imports.timers;
 
-class K2Batt {
-   constructor() {
-	   log ("Init K2Batt");
-      this.aggregateMenu = Panel.statusArea['aggregateMenu'];
-      this.originalIndicator = this.aggregateMenu._power;
-      this.customIndicator = new BaTime.imports.power.Indicator();
-      this.aggregateMenu._indicators.replace_child(
-         this.originalIndicator.indicators,
-         this.customIndicator.indicators
-      );
-   }
-   destroy() {
-	   log ("Destroy K2Batt");
-      this.aggregateMenu._indicators.replace_child(
-         this.customIndicator.indicators,
-         this.originalIndicator.indicators
-      );
-   }
+let batt, button;
+let kBatt = new imports.gi.UPowerGlib.Device();
+let intervalID;
+
+function init() {
+	log(init);
+	kBatt.set_object_path_sync("/org/freedesktop/UPower/devices/keyboard_hid_dco2co26ocdo21o79_battery", null);
+	var percentage = kBatt.percentage;
+	button = new Clutter.Text();
+	button.set_text("K2: " + percentage +"%");
 }
 
-let batt;
-
 function enable() {
-	log ("Enable K2Batt");
-   batt = new K2Batt();
+	Main.panel._rightBox.insert_child_at_index(button, 0);
+	intervalID = Timers.setInterval(refresh, 120000);
 }
 
 function disable() {
-   log ("Disable K2Batt");
-   batt.destroy();
-   batt = null;
+	log("destroy");
+	Main.panel._rightBox.remove_child(button);
+	Timers.clearInterval(intervalID);
+}
+
+function refresh (){
+	log("refresh");
+	kBatt.refresh_sync(null);
+	let percentage = kBatt.percentage;
+	button.set_text("K2: " + percentage +"%");
+	log("sleep 2min");
 }
