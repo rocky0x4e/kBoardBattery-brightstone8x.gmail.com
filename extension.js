dfd8f386304fa839f3c623730924af8fb807646f
Thach Nguyen <thach.nguyen@ebuynow.com>
2019-09-09 12:15:19 +0700

    first beta release

--- extension.js
+++ extension.js
@@ -1,4 +1,3 @@
-const Lng = imports.lang
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const Clutter = imports.gi.Clutter;
@@ -6,12 +5,12 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const Libs = ExtensionUtils.getCurrentExtension();
 var Timers  = Libs.imports.timers;
 var battText;
-var kBatt = new imports.gi.UPowerGlib.Device();
+var kBatt;
 var intervalID;
 
 function init() {
 	log("[K2 batt] init");
-	kBatt.set_object_path_sync("/org/freedesktop/UPower/devices/keyboard_hid_dco2co26ocdo21o79_battery", null);
+	findKeyboard();
 	let Color = new imports.gi.Clutter.Color({red:0, blue:0, green:255, alpha:255});
 	battText = new Clutter.Text(Color);
 	battText.set_color(Color);
@@ -40,4 +39,18 @@ function readBatt (){
 		battText.set_text("K2: N/A");
 	}
 	log("[K2 batt] sleep 2min");
+}
+
+function findKeyboard(){
+	log("[K2 batt] findKeyboard");
+	let upowerClient = imports.gi.UPowerGlib.Client.new_full(null);
+	let devices = upowerClient.get_devices();
+	let i;
+	for (i=0; i < devices.length; i++){
+		let objPath = devices[i].get_object_path();
+		if (objPath.includes("keyboard_hid_")){
+			kBatt = devices[i];
+			break;
+		}
+	}
 }
