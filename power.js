0e7ccf8da2ffb4723366886c805f1abe2c8d1115
Thach Nguyen <thach.nguyen@ebuynow.com>
2019-09-09 17:23:20 +0700

    save

--- power.js
+++ power.js
@@ -2,14 +2,12 @@ const Lang = imports.lang;
 const { Clutter, Gio, St, UPowerGlib: UPower } = imports.gi;
 const BaseIndicator = imports.ui.status.power.Indicator;
 const Power = imports.ui.status.power
-
 const PowerManagerProxy = Gio.DBusProxy.makeProxyWrapper(Power.DisplayDeviceInterface);
-
-var kb ;
-
+var kb;
 var Indicator = class extends BaseIndicator {
 
 	constructor(){
+		log("[k2 batt] new indicator");
 		log("[k2 batt] findKeyboard");
 		let upowerClient = imports.gi.UPowerGlib.Client.new_full(null);
 		let devices = upowerClient.get_devices();
@@ -30,24 +28,27 @@ var Indicator = class extends BaseIndicator {
                                                     return;
                                                 }
                                                 this._proxy.connect('g-properties-changed',
-                                                                    this._sync.bind(this));
+												this._sync.bind(this));
                                                 this._sync();
                                             });
 	}
 	
    _getBatteryStatus() {
 		log("[k2 batt] read battery info");
-		kb.refresh_sync(null);
-		if (kb.has_statistics) {
-			let percentage = "KChron " + kb.percentage +"%";
-			return percentage;
-		} else {
+		try {
+			kb.refresh_sync(null);
+		} catch (err) {
+			log("[k2 batt] no batt found ");
 			return "N/A";
 		}
+		let percentage = "KChron " + kb.percentage +"%";
+		log("[k2 batt] " + percentage);
+		return percentage;		
    }
 
    _sync() {
-      super._sync();
-      this._percentageLabel.clutter_text.set_markup('<span size="smaller">' + this._getBatteryStatus() + '</span>');
+		log("[k2 batt] _sync " + kb.get_object_path());
+		super._sync();
+		this._percentageLabel.clutter_text.set_markup('<span size="smaller">' + this._getBatteryStatus() + '</span>');
    }
 }
