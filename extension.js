7429e0518e1cc7f2a4f36b1bfdc06517c309e3a4
Thach Nguyen <thach.nguyen@ebuynow.com>
2019-09-09 15:26:07 +0700

    beta2

--- extension.js
+++ extension.js
@@ -1,56 +1,35 @@
-const St = imports.gi.St;
-const Main = imports.ui.main;
-const Clutter = imports.gi.Clutter;
 const ExtensionUtils = imports.misc.extensionUtils;
-const Libs = ExtensionUtils.getCurrentExtension();
-var Timers  = Libs.imports.timers;
-var battText;
-var kBatt;
-var intervalID;
+const localLib = ExtensionUtils.getCurrentExtension();
+const Panel = imports.ui.main.panel;
 
-function init() {
-	log("[K2 batt] init");
-	findKeyboard();
-	let Color = new imports.gi.Clutter.Color({red:0, blue:0, green:255, alpha:255});
-	battText = new Clutter.Text(Color);
-	battText.set_color(Color);
+class KBatt {
+   constructor() {
+   log("[k2 batt] new");
+      this.aggregateMenu = Panel.statusArea['aggregateMenu'];
+      this.originalIndicator = this.aggregateMenu._power;
+      this.customIndicator = new localLib.imports.power.Indicator();
+      this.aggregateMenu._indicators.replace_child(
+         this.originalIndicator.indicators,
+         this.customIndicator.indicators
+      );
+   }
+   destroy() {
+		log("[k2 batt] destroy");
+      this.aggregateMenu._indicators.replace_child(
+         this.customIndicator.indicators,
+         this.originalIndicator.indicators
+      );
+   }
 }
 
+let keyboard;
+
 function enable() {
-	log("[K2 batt] enable");
-	Main.panel._rightBox.insert_child_at_index(battText, 0);
-	readBatt();
-	intervalID = Timers.setInterval(readBatt, 120000);
+	log("[k2 batt] enable");
+   keyboard = new KBatt();
 }
 
 function disable() {
-	log("[K2 batt] destroy");
-	Main.panel._rightBox.remove_child(battText);
-	Timers.clearInterval(intervalID);
-}
-
-function readBatt (){
-	log("[K2 batt] read battery info");
-	kBatt.refresh_sync(null);
-	if (kBatt.has_statistics) {
-		let percentage = kBatt.percentage;
-		battText.set_text("K2: " + percentage +"%");
-	} else {
-		battText.set_text("K2: N/A");
-	}
-	log("[K2 batt] sleep 2min");
-}
-
-function findKeyboard(){
-	log("[K2 batt] findKeyboard");
-	let upowerClient = imports.gi.UPowerGlib.Client.new_full(null);
-	let devices = upowerClient.get_devices();
-	let i;
-	for (i=0; i < devices.length; i++){
-		let objPath = devices[i].get_object_path();
-		if (objPath.includes("keyboard_hid_")){
-			kBatt = devices[i];
-			break;
-		}
-	}
+   keyboard.destroy();
+   keyboard = null;
 }
